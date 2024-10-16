d74414c6c02454d45634a53b2f3eb4aa216f26e7
darrell-fashun <70173551+darrell-fashun@users.noreply.github.com>
2024-10-16 11:24:22 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ terraform {
 
 resource "null_resource" "resource1" {
     provisioner "local-exec" {
-    command = "curl 'https://hashicorp.com'"
+    command = "curl -vvv 'https://hashicorp.com'"
 }
 }
 resource "null_resource" "resource2" {
     provisioner "local-exec" {
-    command = "curl 'https://hashicorp.com'"
+    command = "curl -vvv 'https://hashicorp.com'"
 }
 }
